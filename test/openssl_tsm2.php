13eeebdf933fae55e55b8c30afa33e7aecc89f74
lpilp <36149355+lpilp@users.noreply.github.com>
2021-05-20 14:56:26 +0800

    Update openssl_tsm2.php

--- test/openssl_tsm2.php
+++ test/openssl_tsm2.php
@@ -1,5 +1,5 @@
 <?php
-// openssl支持密码对的生成，sm2的加解密，
+// openssl支持密码对的生成
 // 但未支持解签名,证书颁发，openssl 1.1.1 版本目前 不支持 sm3withsm2, 因为sm3withsm2的算法，与普通的sha256 with ecdsa的椭圆算法不一样
 // git上有相关的gmssl或是tassl的基于openssl开发会支持，需要安装然后替换掉当前的openssl, 并且将替换的openssl用源码方式编译到PHP中，较麻烦
 // 操作请参考: http://gmssl.org/docs/php-api.html
