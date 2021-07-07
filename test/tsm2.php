c56de55477faa9ca8089dd3642bd728479e8bfab
lpilp <lpilp@126.com>
2021-07-07 14:13:55 +0800

    修改sm2测试

--- test/tsm2.php
+++ test/tsm2.php
@@ -8,23 +8,30 @@ $userId = '1234567812345678';
 $document = "我爱你ILOVEYOU!";
 
 define('GK',1);
-define('SIGN',0);
+define('SIGN',1);
 define('SIGNPEM',1);
 //返回的签名16进制还是base64, 目前可选hex,与base64两种
 $sm2 = new MySm2('base64');
 
 if(GK){
-    print_r($sm2->generatekey());
-    print_r($sm2->generatePemkey());
+    echo "\n----------生成明文密钥对--------------------------\n";
+    print_r($sm2->generatekey()); //生成明文密钥
+    echo "\n----------生成pem密钥对--------------------------\n";
+    print_r($sm2->generatePemkey()); //生成pem密钥，请放到相应的文件中
 }
+
 if(SIGN){
+    echo "\n---------明文密钥签名---------------------------\n";
     $sign = $sm2->doSign( $document, $privateKey, $userId);
-    print_r($sign."\n");
+    print_r($sign);
+    echo "\n---------明文密钥验签---------------------------\n";
     var_dump($sm2->verifySign( $document, $sign, $publicKey, $userId ));
 }
 
 if(SIGNPEM){
+    echo "\n---------PEM密钥签名---------------------------\n";
     $sign = $sm2->doSignOutKey( $document, $privatefile, $userId);
-    print_r($sign."\n");
+    print_r($sign);
+    echo "\n---------PEM密钥验签---------------------------\n";
     var_dump($sm2->verifySignOutKey( $document, $sign, $publicfile, $userId ));
 }
