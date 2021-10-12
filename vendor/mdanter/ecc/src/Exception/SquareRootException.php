a4e976c16a38e2de94335bc45722e764e2a7716a
lpilp <lpilp@126.com>
2021-10-12 14:14:39 +0800

    delete vendor

--- vendor/mdanter/ecc/src/Exception/SquareRootException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Mdanter\Ecc\Exception;
-
-class SquareRootException extends NumberTheoryException
-{
-
-}
--- vendor/mdanter/ecc/validate_examples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in examples/*.php; do
-    php $i
-    if [ $? != 0 ]; then
-        echo "Error running example code";
-        exit -1
-    fi;
-done
