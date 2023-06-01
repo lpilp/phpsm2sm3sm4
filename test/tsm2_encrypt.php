5a1218560bdec8f447ec760a526be52c2a469b77
lpilp <lpilp@126.com>
2023-06-02 00:15:38 +0800

    更新splsm2

--- test/tsm2_encrypt.php
+++ test/tsm2_encrypt.php
@@ -4,7 +4,8 @@ use Rtgm\sm\RtSm2;
 $sm2 = new RtSm2();
 $publicKey = '043d9d4cc71a285af936b36880fd4d6155c22957cd2c84ea313469065207fb951b9ef1db79d69af8886e91e833da1ebc6bfdde86e70f52923d6e042eaa147624c7'; // 公钥
 $privateKey = 'a7763cd4fe7db2a2146fc09bf2d5e5a30e10c51b7e4bed00b3a26ec79ba78ff3'; // 私钥
-
+$publicKey = '04eb4b8bbe15e3ad94b85196adc2c6f694436b3c1336170fd1daac8b10d2b8824ada9687c138fb81590e0f66ab9678161732ac0d7866b169e76b74483285f2bc04';
+$privateKey = '0bc1c1d2771b64ba1922d72f8a451cd09a82176f74d975d484ec62c862176b75';
 $document = str_repeat('abcdef',10);
 // sm2的非对称加解密，不建议加密太长的字符串
 echo "原始: $document";
--- test/tsplsm2.php
+++ test/tsplsm2.php
@@ -6,11 +6,22 @@ $publicKey = '04eb4b8bbe15e3ad94b85196adc2c6f694436b3c1336170fd1daac8b10d2b8824a
 $privateKey = '0bc1c1d2771b64ba1922d72f8a451cd09a82176f74d975d484ec62c862176b75';
 $userId = '1234567812345678';
 $document = "app_id=test221124213123300000012";
-
-
+// $document .= "xxxxdxx";
+$document = "我爱你ILOVEYOU!";
 $ssm2 = new SimpleSm2($privateKey,$publicKey);
+// 不随机变,就是固定的文本，固定的密钥，生成的固定的签名,
+// 不设置的话缺省是true, 每次的签名都不一样，看对方的需求，有些银行要求每次都变
+// $ssm2->set_rand_sign_flag(false); 
+$ssm2->set_fix_foreignkey_flag(true);
+// var_dump($publicKey);die();
+list($c1,$c3,$c2) = $ssm2->encrypt_raw($publicKey, $document);
 
+var_dump($c1,$c3,$c2);
+$val = $ssm2->decrypt($privateKey, $c1,$c3,$c2);
+var_dump($val);
+// list($r, $s) = $ssm2->sign_raw($document, $privateKey, $publicKey,$userId);
 
-list($r, $s) = $ssm2->sign_raw($document, $privateKey, $publicKey,$userId);
-
-var_dump(gmp_strval($r,16), gmp_strval($s,16));
+// list($r, $s) = $ssm2->sign_raw($document, $privateKey, null,$userId);
+// var_dump($r, $s);
+// $result = $ssm2->verifty_sign_raw($document,$publicKey,$r,$s,$userId);
+// var_dump($result);
